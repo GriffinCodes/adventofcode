92e13230e13a39ebd6a88e1dacc230ca3ae89aff
GriffinCodes <griffin@projecteden.gg>
2023-12-07 11:54:26 -0600

    2023/07

--- 2023/07/solution.ts
+++ 2023/07/solution.ts
@@ -1,27 +1,15 @@
-import { example, NEWLINE, readFile } from "../../shared/util";
+import { example, NEWLINE, readFile } from "../../shared/util"
 
 let part: number
 let cardOrder: string
 
-enum HandType {
-	FIVE_OF_A_KIND,
-	FOUR_OF_A_KIND,
-	FULL_HOUSE,
-	THREE_OF_A_KIND,
-	TWO_PAIR,
-	ONE_PAIR,
-	HIGH_CARD,
-}
-
 class Hand {
 	frequencies: number[] = Array(cardOrder.length).fill(0)
-	type: HandType
 	strength: number
 
 	constructor(public cards: string, public bid: number) {
 		this.frequencies = this.getFrequencies()
-		this.type = this.getHandType()
-		this.strength = this.getHandStrength()
+		this.strength = this.getStrength()
 	}
 
 	getFrequencies() {
@@ -41,48 +29,19 @@ class Hand {
 		return this.frequencies.nonZero()
 	}
 
-	getHandType(): HandType {
-		let countFrequency = (frequency: number): number => {
-			return this.frequencies.filter(freq => freq == frequency).length;
-		}
-
-		if (countFrequency(5) == 1)
-			return HandType.FIVE_OF_A_KIND
-
-		if (countFrequency(4) == 1)
-			return HandType.FOUR_OF_A_KIND
-
-		if (countFrequency(3) == 1 && countFrequency(2) == 1)
-			return HandType.FULL_HOUSE
-
-		if (countFrequency(3) == 1)
-			return HandType.THREE_OF_A_KIND
-
-		if (countFrequency(2) == 2)
-			return HandType.TWO_PAIR
-
-		if (countFrequency(2) == 1)
-			return HandType.ONE_PAIR
-
-		return HandType.HIGH_CARD
+	getStrength() {
+		return this.frequencies.map((frequency, index) => frequency * index).sum();
 	}
 
-	getHandStrength(): number {
-		if (this.type != HandType.HIGH_CARD)
-			return this.type
-
-		return HandType.HIGH_CARD + this.getRankOfCardAtIndex(0)
-	}
-
-	getRankOfCardAtIndex(index: number) {
+	getRankOfCardAt(index: number) {
 		return cardOrder.indexOf(this.cards.charAt(index))
 	}
 
 	compareTo(other: Hand): number {
 		if (this.strength == other.strength)
 			for (let index = 0; index < this.cards.length; index++)
-				if (this.getRankOfCardAtIndex(index) != other.getRankOfCardAtIndex(index))
-					return this.getRankOfCardAtIndex(index) - other.getRankOfCardAtIndex(index)
+				if (this.getRankOfCardAt(index) != other.getRankOfCardAt(index))
+					return this.getRankOfCardAt(index) - other.getRankOfCardAt(index)
 
 		return this.strength - other.strength
 	}
