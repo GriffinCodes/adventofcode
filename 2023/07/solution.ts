d67cb8d8307af33b91994f7a60943aa64c60b0e1
GriffinCodes <griffin@projecteden.gg>
2023-12-07 09:29:14 -0600

    2023/07

--- 2023/07/solution.ts
+++ 2023/07/solution.ts
@@ -1,7 +1,7 @@
 import { example, NEWLINE, readFile } from "../../shared/util";
 
-let part: number;
-let cardOrder: string;
+let part: number
+let cardOrder: string
 
 enum HandType {
 	FIVE_OF_A_KIND,
@@ -14,75 +14,75 @@ enum HandType {
 }
 
 class Hand {
-	frequency: number[] = Array(cardOrder.length).fill(0);
-	type: HandType;
-	strength: number;
+	frequencies: number[] = Array(cardOrder.length).fill(0)
+	type: HandType
+	strength: number
 
 	constructor(public cards: string, public bid: number) {
-		this.frequency = this.determineFrequency();
-		this.type = this.getHandType();
-		this.strength = this.getHandStrength();
+		this.frequencies = this.getFrequencies()
+		this.type = this.getHandType()
+		this.strength = this.getHandStrength()
 	}
 
-	determineFrequency() {
-		let jokers: number = 0;
+	getFrequencies() {
+		let jokers: number = 0
 
 		for (const card of this.cards.split(""))
 			if (part == 2 && card == "J")
 				++jokers
 			else
-				++this.frequency[cardOrder.indexOf(card)];
+				++this.frequencies[cardOrder.indexOf(card)]
 
-		this.frequency.sortNumeric().reverse();
+		this.frequencies.sortNumeric().reverse()
 
 		if (part == 2)
-			this.frequency[0] += jokers
+			this.frequencies[0] += jokers
 
-		return this.frequency.nonZero();
+		return this.frequencies.nonZero()
 	}
 
 	getHandType(): HandType {
 		let countFrequency = (frequency: number): number => {
-			return this.frequency.filter(freq => freq == frequency).length;
+			return this.frequencies.filter(freq => freq == frequency).length;
 		}
 
 		if (countFrequency(5) == 1)
-			return HandType.FIVE_OF_A_KIND;
+			return HandType.FIVE_OF_A_KIND
 
 		if (countFrequency(4) == 1)
-			return HandType.FOUR_OF_A_KIND;
+			return HandType.FOUR_OF_A_KIND
 
 		if (countFrequency(3) == 1 && countFrequency(2) == 1)
-			return HandType.FULL_HOUSE;
+			return HandType.FULL_HOUSE
 
 		if (countFrequency(3) == 1)
-			return HandType.THREE_OF_A_KIND;
+			return HandType.THREE_OF_A_KIND
 
 		if (countFrequency(2) == 2)
-			return HandType.TWO_PAIR;
+			return HandType.TWO_PAIR
 
 		if (countFrequency(2) == 1)
-			return HandType.ONE_PAIR;
+			return HandType.ONE_PAIR
 
-		return HandType.HIGH_CARD;
+		return HandType.HIGH_CARD
 	}
 
 	getHandStrength(): number {
 		if (this.type != HandType.HIGH_CARD)
-			return this.type;
+			return this.type
 
 		return HandType.HIGH_CARD + this.getRankOfCardAtIndex(0)
 	}
 
 	getRankOfCardAtIndex(index: number) {
-		return cardOrder.indexOf(this.cards.charAt(index));
+		return cardOrder.indexOf(this.cards.charAt(index))
 	}
 
 	compareTo(other: Hand): number {
 		if (this.strength == other.strength)
-			for (let i = 0; i < this.cards.length; i++)
-				if (this.getRankOfCardAtIndex(i) != other.getRankOfCardAtIndex(i))
-					return this.getRankOfCardAtIndex(i) - other.getRankOfCardAtIndex(i)
+			for (let index = 0; index < this.cards.length; index++)
+				if (this.getRankOfCardAtIndex(index) != other.getRankOfCardAtIndex(index))
+					return this.getRankOfCardAtIndex(index) - other.getRankOfCardAtIndex(index)
 
 		return this.strength - other.strength
 	}
@@ -96,14 +96,15 @@ let run = () => {
 			.sort((hand1, hand2) => hand1.compareTo(hand2))
 			.reverse()
 			.map((hand, index) => (index + 1) * hand.bid)
-			.sum();
+			.sum()
 
-	console.log(result);
+	console.log(result)
 }
 
 part = 1
 cardOrder = "AKQJT98765432"
 run()
+
 part = 2
-cardOrder = "AKQT98765432J";
+cardOrder = "AKQT98765432J"
 run()
