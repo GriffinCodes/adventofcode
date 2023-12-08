03f1e994b0d3063ce06ec9e9807207c356960f7f
GriffinCodes <griffin@projecteden.gg>
2023-12-07 23:40:24 -0600

    2023/08

---
+++ 2023/08/solution.ts
@@ -0,0 +1,27 @@
+import {DOUBLE_NEWLINE, example, NEWLINE, readFile} from "../../shared/util";
+
+let input: string[] = readFile(example() ? 'example' : 'input').split(DOUBLE_NEWLINE);
+let instructions: string = input.shift();
+
+class Node {
+	static instances: { [name: string]: Node } = {};
+
+	constructor(public name: string, public left: string, public right: string) {
+		Node.instances[name] = this;
+	}
+}
+
+let nodes: Node[] = input.shift().split(NEWLINE).map(line => {
+	for (let matcher of line.matchAll(/(\w+) = \((\w+), (\w+)\)/g))
+		return new Node(matcher[1], matcher[2], matcher[3]);
+});
+
+let node = nodes[0]
+let steps = 0
+
+while (node.name != 'ZZZ') {
+	let instruction = instructions.charAt(steps % instructions.length)
+	node = instruction == 'R' ? Node.instances[node.right] : Node.instances[node.left];
+	++steps;
+}
+console.log(steps);
